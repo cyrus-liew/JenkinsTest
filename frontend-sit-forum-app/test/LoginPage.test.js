51e870d4bf012a5efc590af20d989524dc9e22ce
cyrus <cyrusthedark@gmail.com>
2023-11-03 05:27:37 +0800

    Update LoginPage.test.js

--- frontend-sit-forum-app/test/LoginPage.test.js
+++ frontend-sit-forum-app/test/LoginPage.test.js
@@ -2,10 +2,11 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const { assert } = require('chai');
 const { Options: ChromeOptions } = require('selenium-webdriver/chrome');
 const { expect } = require('chai');
-
+console.log("imports");
 describe('Login Page Validation Test', function () {
 
     let driver;
+    console.log("let");
 
     before(async function () {
         const chromeOptions = new ChromeOptions();
@@ -15,6 +16,8 @@ describe('Login Page Validation Test', function () {
             .forBrowser('chrome')
             .setChromeOptions(chromeOptions)
             .build();
+
+        console.log("before");
     });
 
     it('should display email validation message for an invalid email.', async function () {
@@ -28,6 +31,7 @@ describe('Login Page Validation Test', function () {
 
         expect(isErrorMessageDisplayed).to.equal(true);
     });
+    console.log("1");
 
     it('should display password validation message for an invalid password.', async function () {
         await driver.get('http://localhost:3000/login'); // Replace with your actual URL
@@ -40,6 +44,7 @@ describe('Login Page Validation Test', function () {
 
         expect(isErrorMessageDisplayed).to.equal(true);
     });
+    console.log("2");
 
     it('should display email validation message for an invalid email with a valid password.', async function () {
         await driver.get('http://localhost:3000/login'); // Replace with your actual URL
@@ -55,6 +60,7 @@ describe('Login Page Validation Test', function () {
 
         expect(isErrorMessageDisplayed).to.equal(true);
     });
+    console.log("3");
 
     it('should display password validation message for an invalid password with a valid email.', async function () {
         await driver.get('http://localhost:3000/login'); // Replace with your actual URL
@@ -70,6 +76,7 @@ describe('Login Page Validation Test', function () {
 
         expect(isErrorMessageDisplayed).to.equal(true);
     });
+    console.log("4");
 
     it('should display no error message for a valid password with a valid email.', async function () {
         const output = true;
@@ -93,7 +100,10 @@ describe('Login Page Validation Test', function () {
 
     });
 
+    console.log("4");
+
     after(async function () {
         await driver.quit();
+        console.log("after");
     });
 });
